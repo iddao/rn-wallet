117ad4c208f050bebe3d1cdb3e5fa7627fd3efd2
yuki-js <20838151+yuki-js@users.noreply.github.com>
2022-05-01 15:15:18 +0900

    initial commit

--- App.tsx
+++ App.tsx
@@ -1,22 +1,5 @@
-import { StatusBar } from 'expo-status-bar';
-import { SafeAreaProvider } from 'react-native-safe-area-context';
-
-import useCachedResources from './hooks/useCachedResources';
-import useColorScheme from './hooks/useColorScheme';
-import Navigation from './navigation';
+import { AppRoot } from "./src/AppRoot";
 
 export default function App() {
-  const isLoadingComplete = useCachedResources();
-  const colorScheme = useColorScheme();
-
-  if (!isLoadingComplete) {
-    return null;
-  } else {
-    return (
-      <SafeAreaProvider>
-        <Navigation colorScheme={colorScheme} />
-        <StatusBar />
-      </SafeAreaProvider>
-    );
-  }
+  return <AppRoot />;
 }
